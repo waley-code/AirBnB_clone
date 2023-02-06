3d15b8177ac8dfcdd4d1f1311560e22621d11f08
efezino idisi <enduranceidisi13@gmail.com>
2023-02-06 23:07:15 +0100

    initial folder setup for project

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the base_model module which contains the class BaseModel
+"""
+
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    This defines all common attributes/methods for other classes
+    """
+
+    def __init__(self):
