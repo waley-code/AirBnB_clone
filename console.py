39b8050dc1ec20aec775b79ad4367e5147600bf0
efezino idisi <enduranceidisi13@gmail.com>
2023-02-11 09:42:02 +0100

    documented all methods for 4 files

--- console.py
+++ console.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
+"""
+This is the console module which contains the HBNBCommand class
+"""
 import cmd
-import importlib
+from importlib import import_module
 from models import storage
-class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
-    __models = ('BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review')
-    __data = storage.all()
-    __s = {'BaseModel': "base_model", 'User': "user",'State': "state",
-           'City':"city", 'Amenity': "amenity",
-           'Place':"place", 'Review':"review"}
-    """
-    An empty line + enter shouldn't execute anything
 
+
+class HBNBCommand(cmd.Cmd):
     """
-    
+    This is a simple command interpreter
+
+    Attributes:
+        prompt (str): used to query for the next command
+        models (tuple): all the classes that exist for this project
+        data (dict): all objects that exist in the json file
+        cls_dicts (dict): all classes with their respective module name
+    """
+    prompt = "(hbnb) "
+    __models = ('BaseModel', 'User', 'State', 'City', 'Amenity', 'Place',
+                'Review')
+    __data = storage.all()
+    __cls_dicts = {'BaseModel': "base_model", 'User': "user",
+                   'State': "state", 'City': "city", 'Amenity': "amenity",
+                   'Place': "place", 'Review': "review"}
+
     def do_quit(self, line):
         """
         exit the console
@@ -27,13 +38,18 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, line):
+        """
+        creates a new instance of any of the existing Classes for this
+        project, saves it to the JSON file and prints the id
+        """
         line = line.strip()
         if not line:
             print("** class name missing **")
         elif line not in self.__models:
             print("** class doesn't exist **")
         else:
-            module = importlib.import_module(f'models.{self.__s[line]}')
+            f_name = f'models.{self.__cls_dicts[line]}'
+            module = import_module(f_name)
             Class = getattr(module, line)
             my_model = Class()
             storage.new(my_model)
@@ -41,60 +57,75 @@ class HBNBCommand(cmd.Cmd):
             print(my_model.id)
 
     def do_show(self, line):
+        """
+        prints the string representation of an instance based on the class
+        name and id
+        """
         if not line:
             print("** class name missing **")
             return
-        d = line[:].split()
-        if d[0] not in self.__models:
+        args = line[:].split()
+        if args[0] not in self.__models:
             print("** class doesn't exist **")
-        elif len(d) != 2:
+        elif len(args) != 2:
             print("** instance id missing **")
 
         else:
-            c = line[:].replace(" ", ".")
+            obj_key = line[:].replace(" ", ".")
             for key, value in self.__data.items():
-                if key == c:
+                if key == obj_key:
                     print(value)
                     return
             print("** no instance found **")
 
-    def do_destroy(self,line):
+    def do_destroy(self, line):
+        """
+        deletes an instance based on the class name and id then saves
+        the change into the JSON file
+        """
         if not line:
             print("** class name missing **")
             return
-        d = line[:].split()
-        if d[0] not in self.__models:
+        args = line[:].split()
+        if args[0] not in self.__models:
             print("** class doesn't exist **")
-        elif len(d) != 2:
+        elif len(args) != 2:
             print("** instance id missing **")
 
         else:
-            c = line[:].strip().replace(" ", ".")
+            obj_key = line[:].strip().replace(" ", ".")
             for key in self.__data.keys():
-                if key == c:
+                if key == obj_key:
                     del self.__data[key]
                     storage.save()
                     return
             print("** no instance found **")
 
     def do_all(self, line):
-        out = []
+        """
+        prints all string reprensation of all instances based or not on
+        the class name
+        """
+        str_rep = []
         if line and line in self.__models:
-             for key, value in self.__data.items():
+            for key, value in self.__data.items():
                 if key.split(".")[0] == line:
-                    out.append(str(value))
+                    str_rep.append(str(value))
         elif line and line not in self.__models:
             print("** class doesn't exist **")
             return
         else:
             for value in self.__data.values():
-                out.append(str(value))
+                str_rep.append(str(value))
 
-        print(out)
+        print(str_rep)
 
     def do_update(self, line):
+        """
+        updates an instance based on the class name and id by adding or
+        updating atrribute then save the change into the JSON file
+        """
         dont = ('id', 'updated_at', 'created_at')
-        out = []
         args = line[:].split()
         size = len(args)
         if size == 0:
@@ -136,32 +167,31 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
                 return
 
-        cls_name, id, ki, val = args[0], args[1], args[2], args[3]
-        if ki in dont:
+        cls_name = args[0]
+        ins_id = args[1]
+        atr_name = args[2]
+        atr_val = args[3]
+        if atr_name in dont:
             return
-        val = val.strip('"')
+        atr_val = atr_val.strip('"\'')
         for key, value in self.__data.items():
             check = key.split('.')
-            if check[0] == cls_name and check[1] == id:
+            if check[0] == cls_name and check[1] == ins_id:
                 dic = value.to_dict()
-                if ki in dic.keys():
-                    if type(dic[ki]) == str:
-                        val = str(val)
-                    elif type(dic[ki]) == int:
-                        val = int(val)
-                    elif type(dic[ki]) == float:
-                        val = float(val)
+                if atr_name in dic.keys():
+                    atr_val = type(dic[atr_name])(atr_val)
                 else:
                     try:
-                        val = int(val)
+                        atr_val = int(atr_val)
                     except ValueError:
                         try:
-                            val = float(val)
+                            atr_val = float(atr_val)
                         except ValueError:
-                            val = str(val)
-                dic.update({ki:val})
+                            atr_val = str(atr_val)
+                dic.update({atr_name: atr_val})
                 cls_name = dic['__class__']
-                module = importlib.import_module(f'models.{self.__s[cls_name]}')
+                f_name = f'models.{self.__cls_dicts[cls_name]}'
+                module = import_module(f_name)
                 Class = getattr(module, cls_name)
                 storage.new(Class(**dic))
                 storage.save()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
-
+"""
+This module contains the TestBaseModel class
+"""
 from models.base_model import BaseModel
 from datetime import datetime
 import unittest
-#from unittest.mock import patch
+
 
 class TestBaseModel(unittest.TestCase):
-
+    """
+    all unit test cases for the BaseModel class
+    """
     def setUp(self):
-        self.m = BaseModel()
+        """
+        create instances of the BaseModel class
+        """
+        self.bm = BaseModel()
 
     def tearDown(self):
-        del self.m
-    
-    def test_init_attr(self):
-        self.assertTrue(self.m.id)
-        self.assertIsInstance(self.m.created_at, datetime)
-        self.assertIsInstance(self.m.updated_at, datetime)
+        """
+        deletes all instances created at exit
+        """
+        del self.bm
 
-    def test_str(self):
-        eo = f"[{self.m.__class__.__name__}] ({self.m.id}) {self.m.__dict__}"
-        
-        self.assertEqual(str(self.m), eo)
+    def test_init_(self):
+        """
+        unit test for the __init__ method
+        """
+        self.assertTrue(self.bm.id)
+        self.assertIsInstance(self.bm.created_at, datetime)
+        self.assertIsInstance(self.bm.updated_at, datetime)
 
-    def test_to_dict(self):
-        dic = self.m.to_dict()
-        self.assertIsInstance(dic, dict)
-        self.assertIsInstance(dic['updated_at'], str)
-        self.assertIsInstance(dic['created_at'], str)
-
-    def test_save(self):
-        pre = self.m.updated_at
-        self.m.save()
-        self.assertNotEqual(self.m.updated_at, pre)
-
-    def test_kwargs(self):
-        dic = self.m.to_dict()
+        dic = self.bm.to_dict()
         new = BaseModel(**dic)
         new.name = "Joy"
         new.my_number = 7
@@ -44,6 +40,27 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(new.my_number, 7)
         self.assertIsInstance(new.created_at, datetime)
         self.assertIsInstance(new.updated_at, datetime)
+
+    def test_str(self):
+        """ unit test for the string representation"""
+        eo = f"[{self.bm.__class__.__name__}] ({self.bm.id})"
+        eo_c = f" {self.bm.__dict__}"
+
+        self.assertEqual(str(self.bm), eo + eo_c)
+
+    def test_to_dict(self):
+        """test the dictionary method"""
+        dic = self.bm.to_dict()
+        self.assertIsInstance(dic, dict)
+        self.assertIsInstance(dic['updated_at'], str)
+        self.assertIsInstance(dic['created_at'], str)
+
+    def test_save(self):
+        """ test the save method"""
+        pre = self.bm.updated_at
+        self.bm.save()
+        self.assertNotEqual(self.bm.updated_at, pre)
+
 
 if __name__ == '__main__':
     unittest.main()
