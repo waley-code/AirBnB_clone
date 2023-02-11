32a1952945c5e21192adbc3f18c87eb39fe7138a
waley-code <oyewaleadeosun@yahoo.com>
2023-02-11 10:37:52 +0000

    updated classes with docstrings

--- models/city.py
+++ models/city.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
+    """
+    This creates new City with attr below
+
+    Attributes:
+          state_id (str): state_id of string type
+          name (str): name of string type
+    """
     state_id = ""
     name = ""
