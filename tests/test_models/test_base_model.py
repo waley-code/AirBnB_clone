6038031dfe8bde3ec275de49d89ff9348217946e
waley-code <oyewaleadeosun@yahoo.com>
2023-02-13 08:32:42 +0000

    documented test_save method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -51,7 +51,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(dic['updated_at'], str)
         self.assertIsInstance(dic['created_at'], str)
         
-    def test_save_method(self):
+    def test_save(self):
+        """tests save method"""
         updated_at_before = self.bm.updated_at
         self.bm.save()
         updated_at_after = self.bm.updated_at
